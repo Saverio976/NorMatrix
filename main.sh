42fd81c56b3f29c271339741f1b3fdb62aebe0aa
Xavier Mitault <xavier.mitault@epitech.eu>
2021-11-01 01:02:25 +0100

    [add] main.sh: execute all tests in src

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+##
+## EPITECH PROJECT, 2021
+## NorMatrix
+## File description:
+## execute all shell script in src/
+##
+
+if [[ "$1" -eq "" ]]
+then
+    PREFIX=$PWD
+else
+    PREFIX=$1
+fi
+
+echo "check folder : $PREFIX"
+
+echo "check useless file :"
+src/useless_file.sh $PREFIX
+echo "number of useless file : `src/useless_file.sh | wc -l`"
+echo "check no more than 80 char"
+src/count_columns.sh $PREFIX
+echo "number of lines > 80 char : `src/count_columns.sh | wc -l`"
