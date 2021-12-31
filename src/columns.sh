837e77ddac817c3267228cc895f1b9850221a5e6
Saverio <xavier.mitault@epitech.eu>
2021-12-31 15:32:12 +0100

    [fix] src/columns.sh: update to match only 81 char

--- src/columns.sh
+++ src/columns.sh
@@ -2,4 +2,4 @@
 ## check if first argument contains more than 80 char on all line
 ## if 80 char exit 1
 ## else 0
-grep -e ".\{80,\}" -H -n -I $1 && exit 1 || exit 0
+grep -e ".\{81,\}" -H -n -I $1 && exit 1 || exit 0
