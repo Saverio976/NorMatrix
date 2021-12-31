6527fc81122821c1883c83bd4ae8241c12a52838
Saverio <xavier.mitault@epitech.eu>
2021-12-31 16:29:06 +0100

    [fix] src/nested_branches.py: case where return line in function call too long

--- src/nested_branches.py
+++ src/nested_branches.py
@@ -9,9 +9,11 @@ with open(file) as f: lines: list = f.readlines()
 for i in range(len(lines)):
     line: str = lines[i]
     if line.startswith(" " * 15):
-        if line.endswith(")\n") and ("if (" in lines[i - 1] or
-            "while (" in lines[i - 1] or
-            "for (" in lines[i - 1]):
+        can_continue = line.endswith(");\n")
+        if can_continue or (line.endswith(")\n") and (
+                "if (" in lines[i - 1] or
+                "while (" in lines[i - 1] or
+                "for (" in lines[i - 1])):
             continue
         print(f"{file}:{i}: {line}", end='')
         HAS_ERROR = 1
