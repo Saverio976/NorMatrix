bddcf82a9a77599e843b8dffc14659bc5b92dcef
Saverio <xavier.mitault@epitech.eu>
2021-12-31 16:33:19 +0100

    [add] src/nested_branches.py: case for while

--- src/nested_branches.py
+++ src/nested_branches.py
@@ -9,7 +9,7 @@ with open(file) as f: lines: list = f.readlines()
 for i in range(len(lines)):
     line: str = lines[i]
     if line.startswith(" " * 15):
-        can_continue = line.endswith(");\n")
+        can_continue = line.endswith(");\n") or line.endswith(") {\n")
         if can_continue or (line.endswith(")\n") and (
                 "if (" in lines[i - 1] or
                 "while (" in lines[i - 1] or
