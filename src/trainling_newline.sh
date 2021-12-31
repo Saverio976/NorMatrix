70bfb9267a6ea8773f9da6c9fac04ca436dc0e54
Saverio <xavier.mitault@epitech.eu>
2021-12-31 13:14:01 +0100

    [update] src/trainling_newline.sh: better matchiing

--- src/trainling_newline.sh
+++ src/trainling_newline.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-grep -e "\n\n$$" -e "\n\n\n" -H -n -I $1 && exit 1 || exit  0
+HAS_ERROR=0
+RES=`cat $1 -e`
+echo $RES | grep -e "$ $ $ " -q && HAS_ERROR=1
+if [[ $HAS_ERROR == 1 ]]; then
+	echo $1: only 1 newline between function
+fi
+if [[ `tail $1 -n 1 | cat -e` == "$" ]]; then
+	echo $1: only 1 newline at the end of file
+	HAS_ERROR=1
+fi
+exit $HAS_ERROR
