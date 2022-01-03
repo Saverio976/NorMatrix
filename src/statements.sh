7571a1eb9ac37b72b03ea18f371df9987769bda7
Saverio <xavier.mitault@epitech.eu>
2022-01-03 21:42:27 +0100

    [add] src/statements.sh tests/statements.c: add case where for with multiple ';'

--- src/statements.sh
+++ src/statements.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-NB_ERROR=`grep -e "\(.*;\)\{2,\}" -I -c $1`
+grep -e "\(.*;\)\{2,\}" -H -n $1 > .tmp.txt
+grep -v -e 'for' .tmp.txt > .tmp.tmp.txt
 
+NB_ERROR=`cat .tmp.tmp.txt | wc -l`
 if [[ $NB_ERROR == 0 ]]; then
+	rm .tmp.txt .tmp.tmp.txt
 	exit 0
 else
-	grep -e "\(.*;\)\{2,\}" -H -n -I $1
-	exit $(($NB_ERROR+0))
+	cat .tmp.tmp.txt
+	rm .tmp.txt .tmp.tmp.txt
+	exit $((NB_ERROR+0))
 fi
--- tests/statements.c
+++ tests/statements.c
@@ -1,5 +1,6 @@
 int a(int b, int c)
 {
+    for (int i = 0; i < b; i++);
     if (b == c) {
         b += c; return b;
     }
