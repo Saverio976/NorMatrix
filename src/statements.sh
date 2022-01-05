66f9a85ac884d49b11469b07caeed835088c6a0b
Saverio <xavier.mitault@epitech.eu>
2022-01-05 19:32:55 +0100

    [remove] src/statements.sh: uncessary loop

--- src/statements.sh
+++ src/statements.sh
@@ -6,20 +6,17 @@ cat $1 > $TMP
 sed '/\/\*/,/\*\//d' $TMP > $TMPTMP
 cat $TMPTMP > $TMP
 
-for I in 0..3
-do
-	grep -e "\(.*;\)\{2,\}" -H -n $TMP > $TMPTMP
-	grep -v -e 'for' $TMPTMP > $TMP
-	grep -v -e '".*;.*"' $TMP > $TMPTMP
-	grep -v -e '//.*;.*"' $TMPTMP > $TMP
-done
+grep -e "\(.*;\)\{2,\}" -H -n $TMP > $TMPTMP
+grep -v -e 'for' $TMPTMP > $TMP
+grep -v -e '".*;.*"' $TMP > $TMPTMP
+grep -v -e '//.*;.*"' $TMPTMP > $TMP
 
-NB_ERROR=`cat $TMPTMP | wc -l`
+NB_ERROR=`cat $TMP | wc -l`
 if [[ $NB_ERROR == 0 ]]; then
 	rm $TMP $TMPTMP
 	exit 0
 else
-	cat $TMPTMP
+	cat $TMP
 	rm $TMP $TMPTMP
 	exit $((NB_ERROR+0))
 fi
