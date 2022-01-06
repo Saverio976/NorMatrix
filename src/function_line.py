2052d45c8dc680fd351a65d631318c6522110c72
Saverio <xavier.mitault@epitech.eu>
2022-01-06 19:01:13 +0100

    [fix] src/function_line.py: fix bad var name

--- src/function_line.py
+++ src/function_line.py
@@ -15,7 +15,7 @@ while match_start != 0:
     match_end = filelines.index('\n}', match_start)
     tmp_str: str = filelines[match_start:match_end]
     nb_line = tmp_str.count('\n') - 1
-    if number_line > 20:
+    if nb_line > 20:
         HAS_ERROR += 1
         line_index = filelines.index(tmp_str)
         line_n = filelines.count('\n', 0, match_start)
