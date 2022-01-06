197b452c6e236782134b4a654d5956ff1f9a70e3
Saverio <xavier.mitault@epitech.eu>
2022-01-06 18:50:28 +0100

    [fix] src/function_line.py: add line + 1 because bad line number

--- src/function_line.py
+++ src/function_line.py
@@ -14,16 +14,17 @@ except:
 while match_start != 0:
     match_end = filelines.index('\n}', match_start)
     tmp_str: str = filelines[match_start:match_end]
-    number_line = tmp_str.count('\n') - 1
+    nb_line = tmp_str.count('\n') - 1
     if number_line > 20:
         HAS_ERROR += 1
         line_index = filelines.index(tmp_str)
         line_n = filelines.count('\n', 0, match_start)
         print(
-            f"{file}:{line_n}: number line per function ({number_line} > 20)"
+            f"{file}:{line_n + 1}: number line per function ({nb_line} > 20)"
         )
     try:
         match_start = filelines.index('\n{', match_end)
     except:
         match_start = 0
+
 exit(HAS_ERROR)
