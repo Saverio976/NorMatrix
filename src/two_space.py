05583c8b3799c361ecf9ff9204cd8f3fcb9ee6a9
Saverio <xavier.mitault@epitech.eu>
2022-01-06 18:50:55 +0100

    [fix] src/two_space.py: two space in a comment or a "" will not trigger

--- src/two_space.py
+++ src/two_space.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import re
 
 HAS_ERROR = 0
 
@@ -14,22 +15,21 @@ def check_non_comment_line(line: str, i: int):
     separator = [chr(k) for k in range(ord('a'), ord('z')+1)] + \
                 [chr(k) for k in range(ord('A'), ord('Z')+1)] + \
                 [chr(k) for k in range(ord('0'), ord('9')+1)]
-    try:
-        comment_index = line.index('//')
-    except:
-        comment_index = len(line)
-    try:
-        line.index('/*')
-        IS_IN_COMMENT = True
-    except:
+    line: str = line.split("//")[0]
+    line = line.split("/*")
+    if len(line) == 1:
+        line: str = line[0]
         e = 0
         while e < len(line) and line[e] not in separator:
             e += 1
+        line: str = re.sub(r'".+?"', '', line[e:])
         try:
-            if line[e:].index('  ') < comment_index:
-                HAS_ERROR += 1
-                print(f"{file}:{i + 1}:{line[:-1]}")
+            line.index("  ")
+            HAS_ERROR += 1
+            print(f"{file}:{i + 1}:{line[:-1]}")
         except: pass
+    else:
+        IS_IN_COMMENT = True
 
 for i in range(len(filelines)):
     line: str = filelines[i]
