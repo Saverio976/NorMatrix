fcb9b957fe8b3760329dff77858eda8ea093eb38
Saverio <xavier.mitault@epitech.eu>
2022-01-12 02:25:02 +0100

    [update] exec.sh: call only the main.py

--- exec.sh
+++ exec.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-if [[ "$1" == "" ]]; then
-	python3 $(dirname "$0")/main.py
-	exit $?
-fi
-
-if [[ "$1" != "tests_run" ]]; then
-	echo "USAGE:"
-	echo -e "\t$0 [tests_run]"
-	echo "DESCRIPTION:"
-	echo -e "\tcheck norm!"
-	echo "ARGS:"
-	echo -e "\tNO ARGS:\tcheck the norm in your current working directory"
-	echo -e "\ttests_run:\ttests the norm checker"
-	exit 0
-fi
-
 cd $(dirname "$0")
 
-python3 main.py tests_run
+python3 main.py $@
