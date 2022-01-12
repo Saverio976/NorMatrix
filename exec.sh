8a901a6b1824d14bfd663b3b02e4c8c6a56fae44
Saverio <xavier.mitault@epitech.eu>
2022-01-12 22:50:11 +0100

    [fix] exec.sh: exec without args

--- exec.sh
+++ exec.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+LAST_PWD=$PWD
+ALL_ARGS=$@
+
 cd $(dirname "$0")
 
-python3 main.py $@
+if [[ "$ALL_ARGS" == "" ]]; then
+	python3 main.py $LAST_PWD
+else
+	python3 main.py $ALL_ARGS
+fi
