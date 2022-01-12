e0a57b23a029973907c1262240ed3eefd9ba54de
Saverio <xavier.mitault@epitech.eu>
2022-01-12 22:53:16 +0100

    [fix] normatrix/normatrix/source/print_stats.py: crash if there is no file to check

--- normatrix/normatrix/source/print_stats.py
+++ normatrix/normatrix/source/print_stats.py
@@ -10,13 +10,13 @@ def print_stats(stats: list, files: list) -> None:
             average_dict[elem[0]] = elem[1]
         else:
             average_dict[elem[0]] += elem[1]
-    if len(files) != 0:
-        average = sum(average_dict.values()) / len(files)
     nb_major = len([elem for elem in stats if elem[2] == 0])
     nb_minor = len([elem for elem in stats if elem[2] == 1])
     nb_info = len([elem for elem in stats if elem[2] == 2])
     color.print_color("cyan", f"\nnumber of file checked: {len(files)}")
-    color.print_color("cyan", f"average number of error per file: {average}")
+    if len(files) != 0:
+        average = sum(average_dict.values()) / len(files)
+        color.print_color("cyan", f"average number of error per file: {average}")
     color.print_color("red", f"number of MAJOR: {nb_major} = {-3 * nb_major}")
     color.print_color("blue", f"number of MINOR: {nb_minor} = {-1 * nb_minor}")
     color.print_color("green", f"number of INFO: {nb_info}")
