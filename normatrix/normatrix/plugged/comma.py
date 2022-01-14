158714afd0296b1f360f651c4d74d606b1607bbf
Saverio <xavier.mitault@epitech.eu>
2022-01-14 18:28:41 +0100

    [fix] normatrix/normatrix/plugged/comma.py: bad error for ','

--- normatrix/normatrix/plugged/comma.py
+++ normatrix/normatrix/plugged/comma.py
@@ -13,7 +13,7 @@ def check(file: CFileParse) -> (int, int):
         line = file.sub_parsedline[i]
         if line[0] != TypeLine.COMMENT:
             ll = re.sub('\/\/.*', '', line[1])
-            ll = re.sub("'.'", '', ll)
+            ll = re.sub("'{0,1}(.)'", '', ll)
             m = re.search(",\S", ll)
             if m != None:
                 print(f"{file.basename}:{i + 1}: need a space after a comma ({ll[m.start():]})")
