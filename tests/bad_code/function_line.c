1715a82a38bf513a9ff12083ae6143e24ae1787a
saverio <xavier.mitault@epitech.eu>
2022-01-18 00:42:18 +0100

    [fix] NorMatrix: function parsing as always; find a mistake

--- tests/bad_code/function_line.c
+++ tests/bad_code/function_line.c
@@ -2,25 +2,25 @@ static char aaaaaaaaaaaaaaa(int a,
         int b,
         int c)
 {
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
+    abc;
     return a + b + c;
 }
