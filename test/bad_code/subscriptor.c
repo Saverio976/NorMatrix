03abda5e1cdacb1793bda298510bb8a108017960
saverio <xavier.mitault@epitech.eu>
2022-01-21 17:12:39 +0100

    [add] test/: tests for the new pluggin

---
+++ test/bad_code/subscriptor.c
@@ -0,0 +1,6 @@
+char tab(int b)
+{
+    char tb[5 + 6 ];
+    char tb[ 5 + 6];
+    return ('x');
+}
---
+++ test/ok_code/subscriptor.c
@@ -0,0 +1,8 @@
+char tab(int a)
+{
+    char c[255];
+
+    if (c[45] == 0);
+    if (c[4 + 6] == 0);
+    return (c[4 - 5]);
+}
