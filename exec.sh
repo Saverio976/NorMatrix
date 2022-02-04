ebe3a94c7c5a623bd1b8bd4ac49930fa4caf88cf
saverio <xavier.mitault@epitech.eu>
2022-02-04 13:35:38 +0100

    [fix] exec.sh: error when add arguments to exec.sh

--- exec.sh
+++ exec.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-LAST_PWD=$PWD
 ALL_ARGS=$@
 
-cd $(dirname "$0")
+PY_MAIN_PATH=$(dirname "$0")
 
-if [[ "$ALL_ARGS" == "" ]]; then
-	python3 main.py $LAST_PWD
-else
-	python3 main.py $ALL_ARGS
-fi
+$PY_MAIN_PATH/main.py $ALL_ARGS
