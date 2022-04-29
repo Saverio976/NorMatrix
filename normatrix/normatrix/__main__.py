3f395627e2e709112a6d1542e975354bbf98829a
saverio <xavier.mitault@epitech.eu>
2022-04-29 18:01:12 +0200

    Fix normatrix packages

--- normatrix/normatrix/__main__.py
+++ normatrix/normatrix/__main__.py
@@ -1,3 +1,6 @@
-from normatrix.source.main import main
+try:
+    from normatrix.source.main import main
+except ModuleNotFoundError:
+    from normatrix.normatrix.source.main import main
 
 main()
--- normatrix/normatrix/welcome.py
+++
@@ -1,3 +0,0 @@
-def welcome():
-    print("Thank you for installing normatrix")
-    print("You can use the -h to see its usage")
