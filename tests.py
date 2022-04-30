faa3213357fa75cc12ff59d072ddebcef9666401
saverio <xavier.mitault@epitech.eu>
2022-04-30 21:10:00 +0200

    Refactor tests to unittests (wip)

--- tests.py
+++ tests.py
@@ -8,6 +8,9 @@ from test.indent.tests import TestIndent
 from test.libc_func.tests import TestLibCFunc
 from test.nb_params.tests import TestNbParams
 from test.nested_branches.tests import TestNestedBranches
+from test.newline_at_end_of_file.tests import TestNewlineAtEndOfFile
+from test.number_function.tests import TestNumberFunction
+from test.operators.tests import TestOperators
 
 if __name__ == "__main__":
     unittest.main()
