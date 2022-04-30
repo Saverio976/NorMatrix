525e20805b374cdbf195c3379dd2daf80d32a22f
saverio <xavier.mitault@epitech.eu>
2022-04-30 20:34:14 +0200

    Add file to execute tests

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from test.header.tests import TestHeader
+from test.function_line.tests import TestFunctionLine
+from test.columns.tests import TestColumns
+from test.comma.tests import TestComma
+from test.indent.tests import TestIndent
+
+if __name__ == "__main__":
+    unittest.main()
