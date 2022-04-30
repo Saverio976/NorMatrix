1c9dd5642fa84631bdd6be4e790017348a6656be
saverio <xavier.mitault@epitech.eu>
2022-04-30 11:58:36 +0200

    Add check for comments not start at start of line

--- normatrix/normatrix/plugged/comments.py
+++ normatrix/normatrix/plugged/comments.py
@@ -11,12 +11,20 @@ def check(context, file: CFileParse) -> (int, int, list):
     nb_error = 0
     list_error = []
 
+    if file.filepath.endswith("Makefile"):
+        return (nb_error, 0, list_error)
     for i, line in enumerate(file.sub_parsedline):
+        if line[0] == TypeLine.COMMENT and len(line[1]) >= 1:
+            if line[1].startswith(' ') and line[1].strip(" ")[0] in ["*", "/"]:
+                list_error.append(
+                    (i + 1, f"need comments starts at first column of line")
+                )
+                nb_error += 1
         if line[0] != TypeLine.FUNCTION:
             continue
         if "//" in line[1] or "/*" in line[1]:
             list_error.append(
-                    (i + 1, f"no comments inside functions")
+                (i + 1, f"no comments inside functions")
             )
             nb_error += 1
     return (nb_error, 1, list_error)
